ceb9d09a76f28549d9797b4d573ceaacd35f44bb
ArditaKrasniqi <72756170+ArditaKrasniqi@users.noreply.github.com>
2021-04-07 10:09:27 +0200

    Update usergym.blade.php

--- resources/views/usergym.blade.php
+++ resources/views/usergym.blade.php
@@ -8,34 +8,67 @@
     <style>
         body{
             background-color: #cbd5e0;
+            background-image: url("/public/images/gym_background.jpg");
         }
         span{
             color:#a0aec0;
             display: inline-block;
-            width: 100px;
+            width: 160px;
             background-color: #4a5568;
             margin-bottom: 3px;
             margin-left: 3px;
             padding: 10px;
+            text-align: center;
         }
+        h2,h3{
+            color: darkblue;
+            font-family: Geneva;
+            text-align: center;
+
+        }
+
+        ::placeholder { /* Chrome, Firefox, Opera, Safari 10.1+ */
+            color: #4a5568 ;
+            opacity: 1;
+            text-align: center;/* Firefox */
+        }
+        button{
+            width: 10%;
+            background-color: #4a5568;
+            color: white;
+            padding: 10px 10px;
+            margin: 8px 0;
+            border: none;
+            border-radius: 10px;
+            cursor: pointer;
+        }
+        input[type=text] {
+            padding: 10px;
+            width: 17%;
+            border-radius:10px;
+            border:0;
+            box-shadow:0 0 15px 4px rgba(0,0,0,0.06);
+
+        }
+
     </style>
 
 
-        <h3>GYM MEMBERSHIP SYSTEM</h3>
+        <h2>GYM MEMBERSHIP SYSTEM</h2>
 
         <form method="POST">
 
             @csrf
 
-            <input placeholder="Firstname"
+            <input type="text" placeholder="Firstname"
                    name="fname"/>
-            <input placeholder="Lastname"
+            <input type="text" placeholder="Lastname"
                    name="lname"/>
-            <input placeholder="Birthdate"
+            <input type="text" placeholder="Birthdate"
                    name="bdate"/>
-            <input placeholder="Expiredate"
+            <input type="text" placeholder="Expiredate"
                    name="edate"/>
-            <input placeholder="Profile picture"
+            <input type="text" placeholder="Profile picture"
                    name="ppicture"/>
 
             <button type="submit">Add member</button>
